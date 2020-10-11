65ae31506b22e68aab547830f41bfbe52ea54c7c
ChillerDragon <ChillerDragon@gmail.com>
2020-10-11 14:27:08 +0200

    Add rebuild arg to start script

--- start.sh
+++ start.sh
@@ -8,19 +8,46 @@ ui text
 cores 1
 priority -19
 EOF
+
+function build() {
+    ./gradlew shadowJar
+}
+
+function run() {
+    if [ ! -f $cfg_path ]
+    then
+        echo "$sample_cfg" > "$cfg_path"
+        vim "$cfg_path"
+    fi
+    if [ ! -f $client ]
+    then
+        build
+    fi
+    java -jar $client -config $cfg_path
+}
+
 if ! [ -x "$(command -v java)" ]
 then
   echo 'Error: java is not installed.' >&2
   exit 1
 fi
-if [ ! -f $cfg_path ]
+
+if [ "$1" == "--help" ] || [ "$1" == "-h" ]
 then
-    echo "$sample_cfg" > "$cfg_path"
-    vim "$cfg_path"
-fi
-if [ ! -f $client ]
+    echo "usage: $(basename "$0") [OPTIONS]"
+    echo "options:"
+    echo "<none>    runs client and builds if not found"
+    echo "--build   re builds no matter if built already"
+    exit
+elif [ "$1" == "--build" ]
 then
-    ./gradlew shadowJar
+    build
+    exit
+elif [ "$#" -ne "0" ]
+then
+    echo "unkown argument try --help"
+    exit 1
 fi
-java -jar $client -config $cfg_path
+
+run
 
